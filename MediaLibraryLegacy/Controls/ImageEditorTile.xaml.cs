a5ff71caaba092a92664a07c76d157c44d78e0e0
Jose.Fajardo <fajardo_jf@hotmail.com>
2020-01-05 19:19:42 +1100

    Updated tile logic

--- MediaLibraryLegacy/Controls/ImageEditorTile.xaml.cs
+++ MediaLibraryLegacy/Controls/ImageEditorTile.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using SharedCode.SQLite;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace MediaLibraryLegacy.Controls
 {
@@ -49,13 +39,5 @@ namespace MediaLibraryLegacy.Controls
             }
             tileTest.Visibility = (orderedItems.Count() > 0) ? Visibility.Visible : Visibility.Collapsed;
         }
-
-    }
-
-
-    public class ViewRotatingTile
-    {
-        public Uri Thumbnail { get; set; }
-        public string Title { get; set; }
     }
 }
---
+++ MediaLibraryLegacy/Entities/ViewRotatingTile.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MediaLibraryLegacy
+{
+    public class ViewRotatingTile
+    {
+        public Uri Thumbnail { get; set; }
+        public string Title { get; set; }
+    }
+}
