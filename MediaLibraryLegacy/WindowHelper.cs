7b583c0137df2848bfdf587dc0ea35ae18c13840
Jose.Fajardo <fajardo_jf@hotmail.com>
2020-01-06 19:27:08 +1100

    hacking windowing stuff

--- MediaLibraryLegacy/WindowHelper.cs
+++ MediaLibraryLegacy/WindowHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation;
@@ -21,6 +22,7 @@ namespace MediaLibraryLegacy
         // i.e. the mediaPlayerElement is still running , i can here the media playing
         public static async void OpenWindow(UIElement windowContent, double width, double height, Action finishedOpeningAction) {
             AppWindow appWindow = await AppWindow.TryCreateAsync();
+            appWindow.Title = "desktopxx";
             appWindow.RequestSize(new Size(width, height));
 
             Grid appWindowRootGrid = new Grid();
@@ -38,8 +40,367 @@ namespace MediaLibraryLegacy
                 
             };
 
-            await appWindow.TryShowAsync();
-            finishedOpeningAction?.Invoke();
+            //dynamic appWindowDynamic = appWindow;
+            //var interop = (ICoreWindowInterop)appWindowDynamic;
+            //var handle = interop.WindowHandle;
+
+            //await appWindow.TryShowAsync();
+            //finishedOpeningAction?.Invoke();
+
+            //var foundWindows = AppWindowEx.GetWindows();
+
+            //var foundLegacyWindows = foundWindows.Where(x => x.Title.StartsWith("desktopxx", StringComparison.InvariantCultureIgnoreCase));
+            //if (foundLegacyWindows.Count() == 1) {
+            //    AppWindowEx.SetWindowBottomMost(foundLegacyWindows.First().Handle);
+            //}
         }
     }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WindowInfo
+    {
+        public uint cbSize;
+        public Rect rcWindow;
+        public Rect rcClient;
+        public uint dwStyle;
+        public uint dwExStyle;
+        public uint dwWindowStatus;
+        public uint cxWindowBorders;
+        public uint cyWindowBorders;
+        public ushort atomWindowType;
+        public ushort wCreatorVersion;
+
+        public WindowInfo(Boolean? filler) : this()   // Allows automatic initialization of "cbSize" with "new WINDOWINFO(null/true/false)".
+        {
+            cbSize = (UInt32)(Marshal.SizeOf(typeof(WindowInfo)));
+        }
+
+    }
+
+    public class AppWindowEx
+    {
+        public string Title { get; set; }
+        public IntPtr Handle { get; set; }
+        public WindowInfo WindowInfo { get; private set; }
+
+        public static List<AppWindowEx> GetWindows()
+        {
+            List<AppWindowEx> windows = new List<AppWindowEx>();
+
+            User32.EnumWindows(
+                delegate (IntPtr hWnd, IntPtr lParam)
+                {
+
+                    if (!User32.IsWindowVisible(hWnd)) return true;
+
+                    int length = User32.GetWindowTextLength(hWnd);
+                    if (length == 0) return true;
+
+                    WindowInfo windowInfo = new WindowInfo();
+                    bool success = User32.GetWindowInfo(hWnd, ref windowInfo);
+                    if (success)
+                    {
+                        AppWindowEx appWindow = new AppWindowEx();
+                        appWindow.Handle = hWnd;
+                        appWindow.WindowInfo = windowInfo;
+
+                        if (length == 0) return true;
+                        StringBuilder builder = new StringBuilder(length);
+                        User32.GetWindowText(hWnd, builder, length + 1);
+                        appWindow.Title = builder.ToString();
+
+                        windows.Add(appWindow);
+                    }
+
+                    return true;
+
+                }, IntPtr.Zero);
+            return windows;
+        }
+
+        //[DllImport("user32.dll")]
+        //static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        //static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+        //const UInt32 SWP_NOSIZE = 0x0001;
+        //const UInt32 SWP_NOMOVE = 0x0002;
+        //const UInt32 SWP_NOACTIVATE = 0x0010;
+        //SetWindowPos(Handle, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+
+        public static void SetWindowBottomMost(IntPtr windowHandle)
+        {
+            User32.SetWindowPos(windowHandle, new IntPtr(1), 0, 0, 0, 0, 0x0002 | 0x0010 | 0x0001); // SWP_NOMOVE ,SWP_NOACTIVATE,SWP_NOSIZE & Bottom most.
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public struct MonitorInfo
+    {
+        // size of a device name string
+        private const int CCHDEVICENAME = 32;
+
+        public int StructureSize;
+        public Rect Monitor;
+        public Rect WorkArea;
+        public uint Flags;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
+        public string DeviceName;
+    }
+
+    public enum ShowWindowCommands
+    {
+        /// <summary>
+        /// Hides the window and activates another window.
+        /// </summary>
+        Hide = 0,
+        /// <summary>
+        /// Activates and displays a window. If the window is minimized or 
+        /// maximized, the system restores it to its original size and position.
+        /// An application should specify this flag when displaying the window 
+        /// for the first time.
+        /// </summary>
+        Normal = 1,
+        /// <summary>
+        /// Activates the window and displays it as a minimized window.
+        /// </summary>
+        ShowMinimized = 2,
+        /// <summary>
+        /// Maximizes the specified window.
+        /// </summary>
+        Maximize = 3, // is this the right value?
+        /// <summary>
+        /// Activates the window and displays it as a maximized window.
+        /// </summary>       
+        ShowMaximized = 3,
+        /// <summary>
+        /// Displays a window in its most recent size and position. This value 
+        /// is similar to <see cref="Win32.ShowWindowCommand.Normal"/>, except 
+        /// the window is not activated.
+        /// </summary>
+        ShowNoActivate = 4,
+        /// <summary>
+        /// Activates the window and displays it in its current size and position. 
+        /// </summary>
+        Show = 5,
+        /// <summary>
+        /// Minimizes the specified window and activates the next top-level 
+        /// window in the Z order.
+        /// </summary>
+        Minimize = 6,
+        /// <summary>
+        /// Displays the window as a minimized window. This value is similar to
+        /// <see cref="Win32.ShowWindowCommand.ShowMinimized"/>, except the 
+        /// window is not activated.
+        /// </summary>
+        ShowMinNoActive = 7,
+        /// <summary>
+        /// Displays the window in its current size and position. This value is 
+        /// similar to <see cref="Win32.ShowWindowCommand.Show"/>, except the 
+        /// window is not activated.
+        /// </summary>
+        ShowNA = 8,
+        /// <summary>
+        /// Activates and displays the window. If the window is minimized or 
+        /// maximized, the system restores it to its original size and position. 
+        /// An application should specify this flag when restoring a minimized window.
+        /// </summary>
+        Restore = 9,
+        /// <summary>
+        /// Sets the show state based on the SW_* value specified in the 
+        /// STARTUPINFO structure passed to the CreateProcess function by the 
+        /// program that started the application.
+        /// </summary>
+        ShowDefault = 10,
+        /// <summary>
+        ///  <b>Windows 2000/XP:</b> Minimizes a window, even if the thread 
+        /// that owns the window is not responding. This flag should only be 
+        /// used when minimizing windows from a different thread.
+        /// </summary>
+        ForceMinimize = 11
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WindowPlacement
+    {
+        /// <summary>
+        /// The length of the structure, in bytes. Before calling the GetWindowPlacement or SetWindowPlacement functions, set this member to sizeof(WINDOWPLACEMENT).
+        /// <para>
+        /// GetWindowPlacement and SetWindowPlacement fail if this member is not set correctly.
+        /// </para>
+        /// </summary>
+        public int Length;
+
+        /// <summary>
+        /// Specifies flags that control the position of the minimized window and the method by which the window is restored.
+        /// </summary>
+        public int Flags;
+
+        /// <summary>
+        /// The current show state of the window.
+        /// </summary>
+        public ShowWindowCommands ShowCmd;
+
+        /// <summary>
+        /// The coordinates of the window's upper-left corner when the window is minimized.
+        /// </summary>
+        public Point MinPosition;
+
+        /// <summary>
+        /// The coordinates of the window's upper-left corner when the window is maximized.
+        /// </summary>
+        public Point MaxPosition;
+
+        /// <summary>
+        /// The window's coordinates when the window is in the restored position.
+        /// </summary>
+        public Rect NormalPosition;
+
+        /// <summary>
+        /// Gets the default (empty) value.
+        /// </summary>
+        public static WindowPlacement Default
+        {
+            get
+            {
+                WindowPlacement result = new WindowPlacement();
+                result.Length = Marshal.SizeOf(result);
+                return result;
+            }
+        }
+    }
+
+    [Flags()]
+    public enum SetWindowPosFlags : uint
+    {
+        /// <summary>If the calling thread and the thread that owns the window are attached to different input queues, 
+        /// the system posts the request to the thread that owns the window. This prevents the calling thread from 
+        /// blocking its execution while other threads process the request.</summary>
+        /// <remarks>SWP_ASYNCWINDOWPOS</remarks>
+        AsynchronousWindowPosition = 0x4000,
+        /// <summary>Prevents generation of the WM_SYNCPAINT message.</summary>
+        /// <remarks>SWP_DEFERERASE</remarks>
+        DeferErase = 0x2000,
+        /// <summary>Draws a frame (defined in the window's class description) around the window.</summary>
+        /// <remarks>SWP_DRAWFRAME</remarks>
+        DrawFrame = 0x0020,
+        /// <summary>Applies new frame styles set using the SetWindowLong function. Sends a WM_NCCALCSIZE message to 
+        /// the window, even if the window's size is not being changed. If this flag is not specified, WM_NCCALCSIZE 
+        /// is sent only when the window's size is being changed.</summary>
+        /// <remarks>SWP_FRAMECHANGED</remarks>
+        FrameChanged = 0x0020,
+        /// <summary>Hides the window.</summary>
+        /// <remarks>SWP_HIDEWINDOW</remarks>
+        HideWindow = 0x0080,
+        /// <summary>Does not activate the window. If this flag is not set, the window is activated and moved to the 
+        /// top of either the topmost or non-topmost group (depending on the setting of the hWndInsertAfter 
+        /// parameter).</summary>
+        /// <remarks>SWP_NOACTIVATE</remarks>
+        DoNotActivate = 0x0010,
+        /// <summary>Discards the entire contents of the client area. If this flag is not specified, the valid 
+        /// contents of the client area are saved and copied back into the client area after the window is sized or 
+        /// repositioned.</summary>
+        /// <remarks>SWP_NOCOPYBITS</remarks>
+        DoNotCopyBits = 0x0100,
+        /// <summary>Retains the current position (ignores X and Y parameters).</summary>
+        /// <remarks>SWP_NOMOVE</remarks>
+        IgnoreMove = 0x0002,
+        /// <summary>Does not change the owner window's position in the Z order.</summary>
+        /// <remarks>SWP_NOOWNERZORDER</remarks>
+        DoNotChangeOwnerZOrder = 0x0200,
+        /// <summary>Does not redraw changes. If this flag is set, no repainting of any kind occurs. This applies to 
+        /// the client area, the nonclient area (including the title bar and scroll bars), and any part of the parent 
+        /// window uncovered as a result of the window being moved. When this flag is set, the application must 
+        /// explicitly invalidate or redraw any parts of the window and parent window that need redrawing.</summary>
+        /// <remarks>SWP_NOREDRAW</remarks>
+        DoNotRedraw = 0x0008,
+        /// <summary>Same as the SWP_NOOWNERZORDER flag.</summary>
+        /// <remarks>SWP_NOREPOSITION</remarks>
+        DoNotReposition = 0x0200,
+        /// <summary>Prevents the window from receiving the WM_WINDOWPOSCHANGING message.</summary>
+        /// <remarks>SWP_NOSENDCHANGING</remarks>
+        DoNotSendChangingEvent = 0x0400,
+        /// <summary>Retains the current size (ignores the cx and cy parameters).</summary>
+        /// <remarks>SWP_NOSIZE</remarks>
+        IgnoreResize = 0x0001,
+        /// <summary>Retains the current Z order (ignores the hWndInsertAfter parameter).</summary>
+        /// <remarks>SWP_NOZORDER</remarks>
+        IgnoreZOrder = 0x0004,
+        /// <summary>Displays the window.</summary>
+        /// <remarks>SWP_SHOWWINDOW</remarks>
+        ShowWindow = 0x0040,
+    }
+
+    public class User32
+    {
+        public const int GWL_EXSTYLE = -20;
+        public const int GWL_STYLE = -16;
+
+        #region Monitors
+        #region EnumDisplayMonitors
+        public delegate bool MonitorEnumDelegate(IntPtr hMonitor, IntPtr hdcMonitor, ref Rect lprcMonitor, IntPtr dwData);
+        [DllImport("user32.dll")]
+        public static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, MonitorEnumDelegate lpfnEnum, IntPtr dwData);
+        #endregion
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern bool GetMonitorInfo(IntPtr hMonitor, ref MonitorInfo lpmi);
+        #endregion
+
+        #region Windows
+        #region EnumWindows
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumChildWindows(IntPtr hwndParent, EnumWindowsProc lpEnumFunc, IntPtr lParam);
+        #endregion
+
+        [DllImport("USER32.DLL")]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        [DllImport("USER32.DLL")]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+        [DllImport("USER32.DLL")]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetWindowInfo(IntPtr hwnd, ref WindowInfo pwi);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowPlacement(IntPtr hWnd, ref WindowPlacement lpwndpl);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPlacement(IntPtr hWnd, [In] ref WindowPlacement lpwndpl);
+
+        //[DllImport("user32.dll", SetLastError = true)]
+        //public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int left, int top, int width, int height, SetWindowPosFlags uFlags);
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hwnd, IntPtr hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+        #endregion
+
+
+        #region Hooks
+        [DllImport("user32.dll")]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+        #endregion
+    }
+
+    //// https://stackoverflow.com/questions/34935077/getting-hwnd-off-of-corewindow-object-in-uwp
+    //[ComImport, Guid("45D64A29-A63E-4CB6-B498-5781D298CB4F")]
+    //[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    //interface ICoreWindowInterop
+    //{
+    //    IntPtr WindowHandle { get; }
+    //    bool MessageHandled { set; }
+    //}
 }
