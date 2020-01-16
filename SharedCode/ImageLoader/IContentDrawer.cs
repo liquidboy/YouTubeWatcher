4ac41a08d6f840abddb15a394dcbcc3eb1c1d3fa
Jose.Fajardo <fajardo_jf@hotmail.com>
2020-01-16 21:30:44 +1100

    started adding the native bits from composition

---
+++ SharedCode/ImageLoader/IContentDrawer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Composition;
+
+using Microsoft.Graphics.Canvas;
+
+namespace SharedCode.ImageLoader
+{
+    public interface IContentDrawer
+    {
+        Task Draw(CompositionGraphicsDevice device, Object drawingLock, CompositionDrawingSurface surface, Size size);
+    }
+
+    public delegate void LoadTimeEffectHandler(CompositionDrawingSurface surface, CanvasBitmap bitmap, CompositionGraphicsDevice device);
+}
